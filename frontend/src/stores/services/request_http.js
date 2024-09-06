639c5168aa893a817d6e74ecbf8781582eeac260
carlos18bp <carlos18bp@gmail.com>
2024-09-06 16:03:59 +0000

    fix: Add CSRF token to do POST request

--- frontend/src/stores/services/request_http.js
+++ frontend/src/stores/services/request_http.js
@@ -1,22 +1,43 @@
 import axios from "axios";
 
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
 /**
  * Request endpoint
  * @param {string} method - Type request.
- * @param {string} url - Endpoint.
+ * @param {string} url - Endpoint
  * @param {object} params - Params.
  * @returns {object} - Data and status from endpoint.
  */
-async function makeRequest(method, url, params = null) {
+async function makeRequest(method, url, params = {}) {
+  const csrfToken = getCookie('csrftoken');
+  const headers = {
+    "Content-Type": "application/json",
+    "X-CSRFToken": csrfToken
+  };
+
   try {
     let response;
 
     switch (method) {
       case "GET":
-        response = await axios.get(`/api/${url}`);
+        response = await axios.get(`/api/${url}`, { headers });
         break;
       case "POST":
-        response = await axios.post(`/api/${url}`, params);
+        response = await axios.post(`/api/${url}`, params, { headers });
         break;
       default:
         throw new Error(`Unsupported method: ${method}`);
